57b720666b02bf449c6e9cbcf82374f27bc10d70
Ruan <martinelliruan@gmail.com>
2020-10-03 21:55:27 +0200

    Add counter

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,11 +8,18 @@ import { useEffect, useState } from 'preact/hooks'
 const Home = () => {
   const [flipped, setFlipped] = useState(false)
   const [strategy, setStrategy] = useState(null)
+  const [counter, setCounter] = useState(10 * 60) // seconds
 
   const toggleFlip = () => setFlipped(!flipped)
 
   useEffect(() => {
     setStrategy(sample(strategies))
+
+    const interval = setInterval(() => {
+      setCounter((c) => c - 1)
+    }, 1000)
+
+    return () => clearInterval(interval)
   }, [])
 
   return (
@@ -25,6 +32,10 @@ const Home = () => {
       <div class={`card ${flipped ? 'flip' : 'unflip'}`} onClick={toggleFlip}>
         <div class="card-face card-front"></div>
         <div class="card-face card-back">{strategy}.</div>
+      </div>
+
+      <div class="footer">
+        <p class="text-muted">New card in {counter} seconds.</p>
       </div>
     </div>
   )
